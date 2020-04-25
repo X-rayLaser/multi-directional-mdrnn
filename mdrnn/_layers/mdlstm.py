ff086f7ac9bc63bf2037c3ea0958298236aea547
Eugene <supernovaprotocol@gmail.com>
2020-04-25 19:56:41 +0700

    Almost implemented MDLSTM

--- mdrnn/_layers/mdlstm.py
+++ mdrnn/_layers/mdlstm.py
@@ -28,21 +28,32 @@ class MDLSTM(BaseMDRNN):
 
 class MDLSTMCell:
     def __init__(self, kernel_size, input_dim, ndims, kernel_initializer,
-                 recurrent_initializer, bias_initializer, activation):
-        pass
+                 recurrent_initializer, shared_weight_initializer, activation):
+        self.input_gate = Gate(kernel_size, input_dim, ndims,
+                               kernel_initializer, recurrent_initializer,
+                               shared_weight_initializer, activation)
+        self.forget_gates = []
+        for _ in range(ndims):
+            gate = Gate(kernel_size, input_dim, ndims,
+                        kernel_initializer, recurrent_initializer,
+                        shared_weight_initializer, activation)
+            self.forget_gates.append(gate)
+
+        self.output_gate = Gate(kernel_size, input_dim, ndims,
+                                kernel_initializer, recurrent_initializer,
+                                shared_weight_initializer, activation)
 
     def process(self, x_batch, prev_states, axes):
-        def input_gate():
-            pass
+        b_input = self.input_gate.compute(x_batch, prev_states, axes)
 
-        b_i = input_gate.activate(x_batch, b, s)
+        b_forget = [0] * len(self.forget_gates)
 
-        b_phi = []
-        for d in range(10):
-            f = forget_gate.activate(x_batch, b, s)
-            fs.append(f)
+        for d in axes:
+            b_forget[d] = self.forget_gates[d].compute(x_batch, prev_states, axes)
 
-        s, b = cell.activate(x_batch, b, s, b_phi)
+        b_output = self.output_gate.compute(x_batch, prev_states, axes)
+
+        # todo: compute cell states and outputs
 
     def _compute_input_gate(self, x_batch, b, s):
         pass
@@ -57,12 +68,17 @@ class MDLSTMCell:
         pass
 
 
-class InputGate:
-    def __init__(self, units, input_dim, kernel_initializer,
+class Gate:
+    def __init__(self, units, input_dim, ndims, kernel_initializer,
                  recurrent_initializer, shared_weight_initializer, activation):
         self.units = units
         self.kernel = tf.Variable(kernel_initializer((input_dim, units), dtype=tf.float32))
-        self.recurrent_kernel = tf.Variable(recurrent_initializer((units, units), dtype=tf.float32))
+
+        self.recurrent_kernels = []
+        for _ in range(ndims):
+            w = tf.Variable(recurrent_initializer((units, units), dtype=tf.float32))
+            self.recurrent_kernels.append(w)
+
         self.shared_kernel = tf.Variable(shared_weight_initializer((units,), dtype=tf.float32))
         self.activation = activation
 
@@ -71,8 +87,39 @@ class InputGate:
 
         for axis in axes:
             b, s = prev_states[axis]
-            z_recurrent = tf.add(tf.matmul(b, self.recurrent_kernel), z_recurrent)
+            z_recurrent = tf.add(tf.matmul(b, self.recurrent_kernels[axis]), z_recurrent)
             z_recurrent = tf.add(tf.matmul(s, self.shared_kernel), z_recurrent)
 
         a = tf.add(tf.matmul(x_batch, self.kernel), z_recurrent)
         return self.activation(a)
+
+
+class CellState:
+    def __init__(self, units, input_dim, ndims, kernel_initializer,
+                 recurrent_initializer, activation):
+        self.units = units
+        self.kernel = tf.Variable(kernel_initializer((input_dim, units), dtype=tf.float32))
+
+        self.recurrent_kernels = []
+        for _ in range(ndims):
+            w = tf.Variable(recurrent_initializer((units, units), dtype=tf.float32))
+            self.recurrent_kernels.append(w)
+
+        self.activation = activation
+
+    def compute(self, x_batch, prev_states, axes, b_input, b_forget):
+        z_recurrent = tf.zeros((1, self.units), dtype=tf.float32)
+
+        for axis in axes:
+            b, s = prev_states[axis]
+            z_recurrent = tf.add(tf.matmul(b, self.recurrent_kernels[axis]), z_recurrent)
+
+        a = tf.add(tf.matmul(x_batch, self.kernel), z_recurrent)
+
+        tf.add(tf.multiply(b_input, self.activation(a)),
+               )
+        return self.activation(a)
+
+    def _sum_over_dimensions(self, cell_states, b_forget, axes):
+        # todo: implement this
+        pass
